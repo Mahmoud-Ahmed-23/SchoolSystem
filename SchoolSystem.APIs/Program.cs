193132ce29cff5cf7d5b93898b9bd2dfb4ce76e1
Mahmoud Ahmed <Mahmoud Ahmed@DESKTOP-3CFC79A>
2025-03-03 18:52:23 +0200

    Create Project Architecture (Clean Architecture)

--- SchoolSystem.APIs/Program.cs
+++ SchoolSystem.APIs/Program.cs
@@ -9,7 +9,7 @@ namespace SchoolSystem.APIs
 
             // Add services to the container.
 
-            builder.Services.AddControllers();
+            builder.Services.AddControllers(); 
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
