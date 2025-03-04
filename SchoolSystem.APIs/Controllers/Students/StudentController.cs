95fbf8985bca9f92a52e8fbf1b611b55a54638df
Mahmoud Ahmed <Mahmoud Ahmed@DESKTOP-3CFC79A>
2025-03-04 16:45:36 +0200

    Create GetStudentList EndPoint

---
+++ SchoolSystem.APIs/Controllers/Students/StudentController.cs
@@ -0,0 +1,24 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using SchoolProject.Data.Entities;
+using SchoolSystem.Core.Features.Students.Queries.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SchoolSystem.APIs.Controllers.Students
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class StudentController(IMediator mediator) : ControllerBase
+	{
+
+
+
+		[HttpGet("GetStudentList")]
+		public async Task<ActionResult<List<Student>>> GetStudentList()
+		{
+			var result = await mediator.Send(new GetStudentListQuery());
+			return Ok(result);
+		}
+	}
+}
