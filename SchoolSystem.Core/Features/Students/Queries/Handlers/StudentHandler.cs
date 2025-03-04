af4e0aa5dddb2838c79df5d958e8eda44b90a880
Mahmoud Ahmed <Mahmoud Ahmed@DESKTOP-3CFC79A>
2025-03-04 16:27:00 +0200

    Add Student Query and Handler

---
+++ SchoolSystem.Core/Features/Students/Queries/Handlers/StudentHandler.cs
@@ -0,0 +1,22 @@
+﻿using MediatR;
+using SchoolProject.Data.Entities;
+using SchoolSystem.Core.Features.Students.Queries.Models;
+using SchoolSystem.Service.Abstracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolSystem.Core.Features.Students.Queries.Handlers
+{
+	public class StudentHandler(IStudentService studentService) : IRequestHandler<GetStudentListQuery, List<Student>>
+	{
+		public async Task<List<Student>> Handle(GetStudentListQuery request, CancellationToken cancellationToken)
+		{
+			var students = await studentService.GetStudentsList();
+
+			return students;
+		}
+	}
+}
---
+++ SchoolSystem.Core/Features/Students/Queries/Models/GetStudentListQuery.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using SchoolProject.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolSystem.Core.Features.Students.Queries.Models
+{
+	public class GetStudentListQuery : IRequest<List<Student>>
+	{
+
+	}
+}
